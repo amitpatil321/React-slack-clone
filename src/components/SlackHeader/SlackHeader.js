df955347b5e588f6c537e4a5ee2a78ea0d146268
Amit <amitptil321@gmail.com>
2019-05-30 16:29:35 +0530

    Add addpeople modal call

--- src/components/SlackHeader/SlackHeader.js
+++ src/components/SlackHeader/SlackHeader.js
@@ -3,12 +3,11 @@ import { Button, Row, Col, Icon } from 'antd';
 import { Consumer } from '../../store/store';
 
 import HeaderOptions from '../../containers/SlackHeader/HeaderOptions';
+import AddPeopleModal from '../../containers/SlackHeader/AddPeople';
+
 import './SlackHeader.css';
 
 class SlackHeader extends Component {
-    state = {
-        addPeopleModalVisible : false
-    }
     render() {
         return (
             <Consumer>
@@ -19,14 +18,19 @@ class SlackHeader extends Component {
                     users = [<Icon type="user" key={room.id} />, room.userIds.length]
                     roomName = "#" + room.name
                 }
-                return <Row>
-                    <Col span={20}>
-                        <strong>{roomName}</strong> <span className="seperator"> </span><Button type="link" shape="circle"><small>{users}</small></Button>
-                    </Col>
-                    <Col span={4} className="room-settings">
-                        <HeaderOptions />
-                    </Col>
-                </Row>
+                return (
+                    <>
+                        <Row>
+                            <Col span={20}>
+                                <strong>{roomName}</strong> <span className="seperator"> </span><Button type="link" shape="circle"><small>{users}</small></Button>
+                            </Col>
+                            <Col span={4} className="room-settings">
+                                <HeaderOptions />
+                            </Col>
+                        </Row>
+                        <AddPeopleModal />
+                    </>
+                )
             }}
             </Consumer>
         )
