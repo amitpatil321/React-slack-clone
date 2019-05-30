a1a6e2324911f26a062a27432f720d2eb3048441
Amit <amitptil321@gmail.com>
2019-05-30 22:05:01 +0530

    Add getRoomUsers function

--- src/utils/SlackUtils.js
+++ src/utils/SlackUtils.js
@@ -14,6 +14,21 @@ export function getJoinableUsers(rooms, currentRoom) {
         return allUsers;
 }
 
+// Returns list of users which are member of room
+export function getRoomUsers(rooms, currentRoom){
+    let allUsers = [];
+    // Find general room, as We are storing all the users in 'general' room
+    let generalRoom = filter(rooms, { id: process.env.REACT_APP_CHATKIT_GENERAL_ROOM })
+    if (generalRoom.length)
+        // List all users except which are already member of this room
+        generalRoom[0].users.forEach(eachUser => {
+            if (currentRoom.userIds.includes(eachUser.id.toString()))
+                allUsers.push(eachUser);
+        })
+    return allUsers;
+
+}
+
 // Creates a message to be posted in channel
 export function peopleJoinedMessage(currentUser, channel, people, type) {
     let message = "", myName = "";
