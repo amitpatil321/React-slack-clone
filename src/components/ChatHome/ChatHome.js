c64f927b7e05578997ac6ab1273603e678a1d8fa
Amit <amitptil321@gmail.com>
2019-06-10 15:06:21 +0530

    Replaced Consumer with useContext

--- src/components/ChatHome/ChatHome.js
+++ src/components/ChatHome/ChatHome.js
@@ -1,10 +1,10 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense, useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Layout, Typography, Spin, Icon, Alert } from 'antd';
 import ContentLoader from 'react-content-loader';
 import { Offline } from 'react-detect-offline';
 
-import { Consumer } from 'store/store';
+import { SlackContext } from 'store/store';
 import Sidebar from 'containers/Sidebar';
 import ListMessages from 'containers/Contents/ListMessages';
 import SlackHeader from '../SlackHeader';
@@ -22,130 +22,128 @@ const {
 const { Title } = Typography;
 const antIcon = <Icon type="loading" style={{ fontSize: 24 }} spin />;
 
-const ChatHome = ({ messages }) =>
-    <Consumer>
-        {(context) => {
-            let { user, room, rooms } = context.state;
-            if (Object.keys(user).length && room !== null && rooms.length)
-                return (
-                    <>
-                        <Layout>
-                            <Sider className="sidebar">
-                                <Sidebar />
-                            </Sider>
-                            <Layout>
-                                <Header className="header" >
-                                    <SlackHeader />
-                                </Header>
-                                <Content className="content">
-                                    {/* handle internet offline event */}
-                                    <Offline>
-                                        <Alert message="Your computer seems to be offline. We’ll keep trying to reconnect." type="warning" />
-                                    </Offline>
-                                    {context.state.isLoading.show &&
-                                        <div className="screen-center text-center">
-                                            <Spin indicator={antIcon} tip={context.state.isLoading.message} />
-                                        </div>
-                                    }
-                                    {messages[room.id] && <ListMessages messages={messages[room.id]} />}
-                                </Content>
-                                <Footer className="footer">
-                                    <SendMessage />
-                                </Footer>
-                            </Layout>
-                        </Layout>
-                        <Suspense fallback={""}>
-                            <AddPeopleModal />
-                            <RemovePeopleModal />
-                            <ChannelInfoDrawer />
-                            <AddChannelModal />
-                            <ListChannelsModal />
-                        </Suspense>
-                    </>
-                )
-            return (
+const ChatHome = ({ messages }) => {
+    let context = useContext(SlackContext)
+    let { user, room, rooms } = context.state;
+    if (Object.keys(user).length && room !== null && rooms.length)
+        return (
+            <>
                 <Layout>
                     <Sider className="sidebar">
-                        <ContentLoader
-                            height={160}
-                            width={400}
-                            speed={2}
-                            primaryColor={'#f3f3f3'}
-                            secondaryColor={'#888'}
-                        >
-                            <rect x="122" y="33" rx="4" ry="4" width="187" height="18" />
-                            <circle cx="72" cy="47" r="35" />
-                        </ContentLoader>
-
-                        <ContentLoader
-                            height={340}
-                            width={400}
-                            speed={2}
-                            primaryColor={'#f3f3f3'}
-                            secondaryColor={'#888'}
-                        >
-                            <rect x="25" y="14" rx="4" ry="4" width="187" height="26" />
-                            <rect x="25" y="70" rx="4" ry="4" width="267" height="26" />
-                            <rect x="25" y="127" rx="4" ry="4" width="267" height="26" />
-                            <rect x="25" y="187" rx="4" ry="4" width="267" height="26" />
-                            <rect x="25" y="247" rx="4" ry="4" width="267" height="26" />
-                        </ContentLoader>
-
-                        <ContentLoader
-                            height={100}
-                            width={400}
-                            speed={2}
-                            primaryColor="#f3f3f3"
-                            secondaryColor="#888"
-                        >
-                            <rect x="25" y="15" rx="4" ry="4" width="250" height="26" />
-                        </ContentLoader>
-
-                        <ContentLoader
-                            height={340}
-                            width={400}
-                            speed={2}
-                            primaryColor={'#f3f3f3'}
-                            secondaryColor={'#888'}
-                        >
-                            <rect x="25" y="14" rx="4" ry="4" width="187" height="26" />
-                            <rect x="25" y="70" rx="4" ry="4" width="267" height="26" />
-                            <rect x="25" y="127" rx="4" ry="4" width="267" height="26" />
-                            <rect x="25" y="187" rx="4" ry="4" width="267" height="26" />
-                            <rect x="25" y="247" rx="4" ry="4" width="267" height="26" />
-                        </ContentLoader>
+                        <Sidebar />
                     </Sider>
                     <Layout>
                         <Header className="header" >
-                            <ContentLoader
-                                height={30}
-                                width={400}
-                                speed={2}
-                                primaryColor="#f3f3f3"
-                                secondaryColor="#888"
-                            >
-                                <rect x="0" y="8" rx="3" ry="3" width="30" height="3" />
-                                <circle cx="35" cy="10" r="3" />
-
-                                <circle cx="93%" cy="10" r="3" />
-                                <circle cx="97%" cy="10" r="3" />
-                            </ContentLoader>
+                            <SlackHeader />
                         </Header>
                         <Content className="content">
-                            <div className="screen-center text-center">
-                                <Title level={3}>Thanks you for using Slack Clone. We appreciate it!</Title>
-                                <p className="text-light">- Your friends at slack Clone</p>
-                                <Spin indicator={antIcon} />
-                            </div>
+                            {/* handle internet offline event */}
+                            <Offline>
+                                <Alert message="Your computer seems to be offline. We’ll keep trying to reconnect." type="warning" />
+                            </Offline>
+                            {context.state.isLoading.show &&
+                                <div className="screen-center text-center">
+                                    <Spin indicator={antIcon} tip={context.state.isLoading.message} />
+                                </div>
+                            }
+                            {messages[room.id] && <ListMessages messages={messages[room.id]} />}
                         </Content>
                         <Footer className="footer">
-                            Footer
+                            <SendMessage />
                         </Footer>
                     </Layout>
                 </Layout>
-            )
-        }}
-    </Consumer>
+                <Suspense fallback={""}>
+                    <AddPeopleModal />
+                    <RemovePeopleModal />
+                    <ChannelInfoDrawer />
+                    <AddChannelModal />
+                    <ListChannelsModal />
+                </Suspense>
+            </>
+        )
+    return (
+        <Layout>
+            <Sider className="sidebar">
+                <ContentLoader
+                    height={160}
+                    width={400}
+                    speed={2}
+                    primaryColor={'#f3f3f3'}
+                    secondaryColor={'#888'}
+                >
+                    <rect x="122" y="33" rx="4" ry="4" width="187" height="18" />
+                    <circle cx="72" cy="47" r="35" />
+                </ContentLoader>
+
+                <ContentLoader
+                    height={340}
+                    width={400}
+                    speed={2}
+                    primaryColor={'#f3f3f3'}
+                    secondaryColor={'#888'}
+                >
+                    <rect x="25" y="14" rx="4" ry="4" width="187" height="26" />
+                    <rect x="25" y="70" rx="4" ry="4" width="267" height="26" />
+                    <rect x="25" y="127" rx="4" ry="4" width="267" height="26" />
+                    <rect x="25" y="187" rx="4" ry="4" width="267" height="26" />
+                    <rect x="25" y="247" rx="4" ry="4" width="267" height="26" />
+                </ContentLoader>
+
+                <ContentLoader
+                    height={100}
+                    width={400}
+                    speed={2}
+                    primaryColor="#f3f3f3"
+                    secondaryColor="#888"
+                >
+                    <rect x="25" y="15" rx="4" ry="4" width="250" height="26" />
+                </ContentLoader>
+
+                <ContentLoader
+                    height={340}
+                    width={400}
+                    speed={2}
+                    primaryColor={'#f3f3f3'}
+                    secondaryColor={'#888'}
+                >
+                    <rect x="25" y="14" rx="4" ry="4" width="187" height="26" />
+                    <rect x="25" y="70" rx="4" ry="4" width="267" height="26" />
+                    <rect x="25" y="127" rx="4" ry="4" width="267" height="26" />
+                    <rect x="25" y="187" rx="4" ry="4" width="267" height="26" />
+                    <rect x="25" y="247" rx="4" ry="4" width="267" height="26" />
+                </ContentLoader>
+            </Sider>
+            <Layout>
+                <Header className="header" >
+                    <ContentLoader
+                        height={30}
+                        width={400}
+                        speed={2}
+                        primaryColor="#f3f3f3"
+                        secondaryColor="#888"
+                    >
+                        <rect x="0" y="8" rx="3" ry="3" width="30" height="3" />
+                        <circle cx="35" cy="10" r="3" />
+
+                        <circle cx="93%" cy="10" r="3" />
+                        <circle cx="97%" cy="10" r="3" />
+                    </ContentLoader>
+                </Header>
+                <Content className="content">
+                    <div className="screen-center text-center">
+                        <Title level={3}>Thanks you for using Slack Clone. We appreciate it!</Title>
+                        <p className="text-light">- Your friends at slack Clone</p>
+                        <Spin indicator={antIcon} />
+                    </div>
+                </Content>
+                <Footer className="footer">
+
+                </Footer>
+            </Layout>
+        </Layout>
+    )
+}
 
 // ChatHome.propType = {
 //     user : PropTypes.object.isRequired,
--- src/components/Content/ListMessages/ListMessages.js
+++ src/components/Content/ListMessages/ListMessages.js
@@ -1,46 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Row, Col } from 'antd';
 import { Comment, Avatar } from 'antd';
 import moment from 'moment';
 import { getUserName, getUserPic } from 'utils/SlackUtils';
 
-import { Consumer } from 'store/store';
+import { SlackContext } from 'store/store';
 
 import './ListMessages.css'
 
 // TODO :  Combine messages from same auther only if they are sent in a span of given time, starting from first message from same author
 const ListMessages = ({ canGroup, message }) => {
+    let { state } = useContext(SlackContext)
+
     if (message == undefined) return false;
 
     let { id, senderId, text, createdAt } = message;
     let time = <small>{moment(createdAt).format("hh:mm A")}</small>;
+    let author = getUserName(state.room, senderId);
+    let avatar = getUserPic(state.room, senderId);
 
     return (
-        (canGroup)
+            (canGroup)
             ?
-            <Row className="grouped-message" key={id}>
-                <Col span={1} style={{ width: "55px" }}>
-                    {time}
-                </Col>
-                <Col span={21}>
-                    <span className="message-text">{text}</span>
-                </Col>
-            </Row>
+                <Row className="grouped-message" key={id}>
+                    <Col span={1} style={{ width: "55px" }}>
+                        {time}
+                    </Col>
+                    <Col span={21}>
+                        <span className="message-text">{text}</span>
+                    </Col>
+                </Row>
             :
-            <Consumer >
-                {({ state }) => {
-                    let author = getUserName(state.room, senderId);
-                    let avatar = getUserPic(state.room, senderId);
-
-                    return <Comment
-                        key={id}
-                        author={<div className="message-sender">{author} {time}</div>}
-                        avatar={<Avatar src={avatar} alt={author} />}
-                        content={<span className="message-text">{text}</span>}
-                    >
-                    </Comment>
-                }}
-            </Consumer>
+                <Comment
+                    key    = {id}
+                    author = {<div className = "message-sender">{author} {time}</div>}
+                    avatar = {<Avatar src = {avatar} alt = {author} />}
+                    content= {<span className="message-text">{text}</span>}
+                >
+                </Comment>
         )
     }
 
