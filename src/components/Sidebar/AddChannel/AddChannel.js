18ba18cf261fa145ebfb7c9e3224ff5aab6d6ef0
Amit <amitptil321@gmail.com>
2019-06-23 15:45:04 +0530

    Feature : Check if channel name or user with same name exist while creating new channel

--- src/components/Sidebar/AddChannel/AddChannel.js
+++ src/components/Sidebar/AddChannel/AddChannel.js
@@ -5,64 +5,64 @@ import './AddChannel.css';
 
 //TODO : check if channel name already exists
 const AddChannel = ({
-    error,
-    addChannelModalVisible,
-    allUsers,
-    hideModal,
-    validationError,
-    channelName,
-    selectedUsers,
-    isPrivate,
-    onSubmit,
-    onInputChange,
-    handleDropdownChange,
-    onCheckboxChange
+	error,
+	addChannelModalVisible,
+	allUsers,
+	hideModal,
+	validationError,
+	channelName,
+	selectedUsers,
+	isPrivate,
+	onSubmit,
+	onInputChange,
+	handleDropdownChange,
+	onCheckboxChange
 }) => {
-    return (
-        <Modal
-            title         = "Add Channel"
-            visible       = {addChannelModalVisible}
-            okButtonProps = {{ form: "frmAddChannel", htmlType: "submit" }}
-            onCancel      = {hideModal}
-            width         = {350}
-            okText        = "Create"
-            maskClosable  = {false}
-        >
-            {(error) ? <Alert message={error} type="error" showIcon/> : ''}
-            <Form id="frmAddChannel" onSubmit={onSubmit} className="login-form">
-                <Form.Item className={(validationError ? 'has-error' : '')}>
-                    <Input
-                        name        = "channel_name"
-                        maxLength   = {25}
-                        onChange    = {onInputChange}
-                        value       = {channelName}
-                        placeholder = "Channel name *"
-                    />
-                    {validationError && <div className="ant-form-explain">Please enter channel name</div>}
-                </Form.Item>
-                <Form.Item>
-                    <Select
-                        name        = "users"
-                        mode        = "multiple"
-                        style       = {{ width: '100%' }}
-                        placeholder = "Add users"
-                        onChange    = {handleDropdownChange}
-                        value       = {selectedUsers}
-                        allowClear  = {true}
-                    >
-                        {allUsers}
-                    </Select>
-                </Form.Item>
-                <Form.Item>
-                    <Checkbox
-                        name     = "private"
-                        onChange = {onCheckboxChange}
-                        checked  = {isPrivate}
-                    >Private Channel</Checkbox>
-                </Form.Item>
-            </Form>
-        </Modal>
-    )
+	return (
+		<Modal
+			title         = "Add Channel"
+			visible       = {addChannelModalVisible}
+			okButtonProps = {{ form: "frmAddChannel", htmlType: "submit" }}
+			onCancel      = {hideModal}
+			width         = {350}
+			okText        = "Create"
+			maskClosable  = {false}
+		>
+			{(error) ? <Alert message={error} type="error" showIcon/> : ''}
+			<Form id="frmAddChannel" onSubmit={onSubmit} className="login-form">
+				<Form.Item className={(validationError ? 'has-error' : '')}>
+					<Input
+						name        = "channel_name"
+						maxLength   = {25}
+						onChange    = {onInputChange}
+						value       = {channelName}
+						placeholder = "Channel name *"
+					/>
+					{validationError && <div className="ant-form-explain">Please enter channel name</div>}
+				</Form.Item>
+				<Form.Item>
+					<Select
+						name        = "users"
+						mode        = "multiple"
+						style       = {{ width: '100%' }}
+						placeholder = "Add users"
+						onChange    = {handleDropdownChange}
+						value       = {selectedUsers}
+						allowClear  = {true}
+					>
+						{allUsers}
+					</Select>
+				</Form.Item>
+				<Form.Item>
+					<Checkbox
+						name     = "private"
+						onChange = {onCheckboxChange}
+						checked  = {isPrivate}
+					>Private Channel</Checkbox>
+				</Form.Item>
+			</Form>
+		</Modal>
+	)
 }
 
 export default AddChannel;
--- src/containers/Sidebar/AddChannel/AddChannelContainer.js
+++ src/containers/Sidebar/AddChannel/AddChannelContainer.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Select } from 'antd';
 import { connect } from 'react-redux';
+import { filter } from 'lodash';
 
 import { joinRoom, hideAddChannel } from 'store/SlackActions';
 import { getAllUsers, peopleJoinedMessage } from 'utils/SlackUtils';
@@ -10,99 +11,150 @@ import AddChannel from 'components/Sidebar/AddChannel';
 const Option = Select.Option;
 
 class AddChannelContainer extends Component {
+	state = {
+		error: null,
+		addChannel: false,
+		channelName: '',
+		validationError: false,
+		isPrivate: false,
+		selectedUsers: []
+	};
 
-    state = {
-        error          : null,
-        addChannel     : false,
-        channelName    : '',
-        validationError: false,
-        isPrivate      : false,
-        selectedUsers  : []
-    }
+	// Returns all users as dropdown value
+	_getAllUsers = () => {
+		let { user } = this.props;
+		let allUsers = getAllUsers(user);
+		if (allUsers) {
+			return allUsers.map(({ id, name }) => {
+				// List all users except logged in user, Coz he will be default member of channel
+				if (user.id !== id) return <Option key={id}>{name}</Option>;
+			});
+		}
+	};
 
-    // Returns all users as dropdown value
-    _getAllUsers = () => {
-        let { user } =  this.props;
-        let allUsers = getAllUsers(user);
-        if(allUsers){
-            return allUsers.map(({id, name}) => {
-                // List all users except logged in user, Coz he will be default member of channel
-                if (user.id !== id)
-                    return <Option key={id}>{name}</Option>;
-            })
-        }
-    }
+	// Handle form submit
+	_onSubmitForm = event => {
+		event.preventDefault();
+		let { user, room, rooms } = this.props;
+		let { channelName, isPrivate, selectedUsers } = this.state;
+		// Check if room with same name already exists in channels user name or user group
+		let channelExists = rooms.filter(
+			room => room.name.toLowerCase() === channelName.trim().toLowerCase()
+		);
+		let userExists = Object.values(room.userStore.users).filter(
+			user => user.name.toLowerCase() === channelName.trim().toLowerCase()
+		);
 
-    // Handle form submit
-    _onSubmitForm = (event) => {
-        event.preventDefault()
-        let { user } = this.props;
-        let { channelName, isPrivate, selectedUsers } = this.state;
-        if (channelName.trim().length) {
-            // Create room
-            createRoom(user, channelName.toLowerCase(), selectedUsers, isPrivate, null , (newRoom) => {
-                // Create message and post to new channel
-                let message = peopleJoinedMessage(user, newRoom.name, selectedUsers, "NEW");
+		console.log(channelExists[0], userExists[0]);
 
-                if (message)
-                // send messge to newly created channel
-                sendMessage(user, newRoom.id, message, () => {}, () => { console.log("Error sendind people joined message"); })
-                setTimeout(() => {
-                    // Set newly created channel as active channel
-                    this.props.joinRoom(newRoom)
-                    // document.querySelector(".channel-"+newRoom.id).click()
-                }, 500);
-                // clear form text
-                this.setState({ channelName: '', isPrivate: false, selectedUsers: [], error: null })
-                // Hide modal
-                this._closeModal()
-            }, (error) => {
-                this.setState({ error : error.error });
-            })
-        } else
-            this.setState({ validationError: true })
-    }
-    _closeModal = () => {
-        // Clear validation error and form on modal close
-        this.setState({ validationError: false, error: null, channelName: '', selectedUsers: [], isPrivate : false })
-        this.props.hideAddChannel()
-    }
-    _onInputChange = (event) => this.setState({ channelName: event.target.value })
-    _onCheckboxChange = (event) => this.setState({ isPrivate: event.target.checked })
-    _onHandleDropdownChange = (selectedUsers) => this.setState({ selectedUsers: selectedUsers })
-    render() {
-        let { addChannelModalVisible } = this.props;
-        return (
-            <AddChannel
-                error                = {this.state.error}
-                addChannelModalVisible={addChannelModalVisible}
-                allUsers             = {this._getAllUsers()}
-                validationError      = {this.state.validationError}
-                channelName          = {this.state.channelName}
-                selectedUsers        = {this.state.selectedUsers}
-                isPrivate            = {this.state.isPrivate}
-                hideModal            = {this._closeModal}
-                onSubmit             = {this._onSubmitForm}
-                onInputChange        = {this._onInputChange}
-                handleDropdownChange = {this._onHandleDropdownChange}
-                onCheckboxChange     = {this._onCheckboxChange}
-            />
-        )
-    }
+		if (!channelExists[0] && !userExists[0]) {
+			if (channelName.trim().length) {
+				// Create room
+				createRoom(
+					user,
+					channelName.toLowerCase(),
+					selectedUsers,
+					isPrivate,
+					null,
+					newRoom => {
+						// Create message and post to new channel
+						let message = peopleJoinedMessage(
+							user,
+							newRoom.name,
+							selectedUsers,
+							'NEW'
+						);
+
+						if (message)
+							// send messge to newly created channel
+							sendMessage(
+								user,
+								newRoom.id,
+								message,
+								() => {},
+								() => {
+									console.log('Error sendind people joined message');
+								}
+							);
+						setTimeout(() => {
+							// Set newly created channel as active channel
+							this.props.joinRoom(newRoom);
+							// document.querySelector(".channel-"+newRoom.id).click()
+						}, 500);
+						// clear form text
+						this.setState({
+							channelName: '',
+							isPrivate: false,
+							selectedUsers: [],
+							error: null
+						});
+						// Hide modal
+						this._closeModal();
+					},
+					error => {
+						this.setState({ error: error.error });
+					}
+				);
+			} else this.setState({ validationError: true });
+		} else
+			this.setState({
+				error: "name '" + channelName + "' is already taken by channel or user."
+			});
+	};
+	_closeModal = () => {
+		// Clear validation error and form on modal close
+		this.setState({
+			validationError: false,
+			error: null,
+			channelName: '',
+			selectedUsers: [],
+			isPrivate: false
+		});
+		this.props.hideAddChannel();
+	};
+	_onInputChange = event => this.setState({ channelName: event.target.value });
+	_onCheckboxChange = event =>
+		this.setState({ isPrivate: event.target.checked });
+	_onHandleDropdownChange = selectedUsers =>
+		this.setState({ selectedUsers: selectedUsers });
+	render() {
+		let { addChannelModalVisible } = this.props;
+		return (
+			<AddChannel
+				error={this.state.error}
+				addChannelModalVisible={addChannelModalVisible}
+				allUsers={this._getAllUsers()}
+				validationError={this.state.validationError}
+				channelName={this.state.channelName}
+				selectedUsers={this.state.selectedUsers}
+				isPrivate={this.state.isPrivate}
+				hideModal={this._closeModal}
+				onSubmit={this._onSubmitForm}
+				onInputChange={this._onInputChange}
+				handleDropdownChange={this._onHandleDropdownChange}
+				onCheckboxChange={this._onCheckboxChange}
+			/>
+		);
+	}
 }
 
-const mapStateToProps = ({ user, addChannelModalVisible }) => {
-    return {
-        user,
-        addChannelModalVisible
-    }
-}
+const mapStateToProps = state => {
+	return {
+		user: state.user,
+		room: state.room,
+		rooms: state.rooms,
+		addChannelModalVisible: state.addChannelModalVisible
+	};
+};
 
 const mapDispatchToProps = dispatch => {
-    return {
-        joinRoom: (room) => dispatch(joinRoom(room)),
-        hideAddChannel: () => dispatch(hideAddChannel()),
-    }
-}
+	return {
+		joinRoom: room => dispatch(joinRoom(room)),
+		hideAddChannel: () => dispatch(hideAddChannel())
+	};
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(AddChannelContainer);
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(AddChannelContainer);
