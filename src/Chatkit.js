f48eb26c48f404b3466231be1c402dddf044a3ec
Amit <amitptil321@gmail.com>
2019-06-23 20:06:19 +0530

    Prettier formatting

--- src/Chatkit.js
+++ src/Chatkit.js
@@ -1,5 +1,5 @@
-import Chatkit from '@pusher/chatkit-client';
-import { notification } from 'antd';
+import Chatkit from "@pusher/chatkit-client";
+import { notification } from "antd";
 
 //TODO : replace token provider with node token auth function
 const tokenProvider = new Chatkit.TokenProvider({
@@ -9,15 +9,15 @@ const tokenProvider = new Chatkit.TokenProvider({
 export default (userId, actions) => {
 	new Chatkit.ChatManager({
 		instanceLocator: process.env.REACT_APP_CHATKIT_INSTANCE_LOCATOR,
-		userId         : userId, // We have stored googleId as userId
-		tokenProvider  : tokenProvider
+		userId: userId, // We have stored googleId as userId
+		tokenProvider: tokenProvider
 	})
 	// Register global events
 		.connect({
-			onAddedToRoom      : actions.subscribeToRoom,
-			onRemovedFromRoom  : actions.removedFromRoom,
-			onPresenceChanged  : actions.setUserPresence,
-			onRoomDeleted      : actions.roomDeleted
+			onAddedToRoom: actions.subscribeToRoom,
+			onRemovedFromRoom: actions.removedFromRoom,
+			onPresenceChanged: actions.setUserPresence,
+			onRoomDeleted: actions.roomDeleted
 		})
 		.then(user => {
 			// Subscribe to all rooms the user is a member of
@@ -27,34 +27,33 @@ export default (userId, actions) => {
 						roomId: room.id,
 						messageLimit: 5,
 						hooks: {
-							onUserJoined       : actions.refresh,
-							onMessage          : actions.addMessage,
-							onUserLeft         : actions.refresh, // force update app on user removal
+							onUserJoined: actions.refresh,
+							onMessage: actions.addMessage,
+							onUserLeft: actions.refresh, // force update app on user removal
 							onUserStartedTyping: actions.isTyping,
 							onUserStoppedTyping: actions.notTyping
-						},
+						}
 					})
 				)
 			).then(rooms => {
-				actions.setUser(user)
+				actions.setUser(user);
 				// Join the first room in the users room list
-				user.rooms.length > 0 && actions.joinRoom(user.rooms[0])
+				user.rooms.length > 0 && actions.joinRoom(user.rooms[0]);
 				// Notify app that Connection is successfull
 				actions.connected(user.rooms);
-			})
+			});
 		})
 		.catch(error => {
-			if (error.info){
+			if (error.info) {
 				notification.error({
-					message    : error.info.error,
+					message: error.info.error,
 					description: error.info.error_description,
-					onClose    : () => {
+					onClose: () => {
 						console.log("inside onClose");
 						// Clearn localstorage
 						localStorage.removeitem("slackUser");
 					}
 				});
-			}else
-				actions.error(actions)
+			} else actions.error(actions);
 		});
-}
+};
