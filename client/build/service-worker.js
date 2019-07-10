110878872728d5dac170acccf4e3f072cc905b88
Amit <amitptil321@gmail.com>
2019-07-11 01:27:21 +0530

    New build

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4b0ee971139f57e982fc339d2ad17a8c.js"
+  "/precache-manifest.4685a827264eaaf963953f06517ede8c.js"
 );
 
 self.addEventListener('message', (event) => {
--- client/src/Chatkit.js
+++ client/src/Chatkit.js
@@ -1,59 +1,56 @@
-import Chatkit from "@pusher/chatkit-client";
-import { notification } from "antd";
+import Chatkit from '@pusher/chatkit-client';
+import { notification } from 'antd';
 
-//TODO : replace token provider with node token auth function
+// TODO : replace token provider with node token auth function
 const tokenProvider = new Chatkit.TokenProvider({
-	url: `https://us1.pusherplatform.io/services/chatkit_token_provider/v1/${process.env.REACT_APP_CHATKIY_INSTANCE_ID}/token`
+  url: `https://us1.pusherplatform.io/services/chatkit_token_provider/v1/${process.env.REACT_APP_CHATKIY_INSTANCE_ID}/token`,
 });
 
 export default (userId, actions) => {
-	new Chatkit.ChatManager({
-		instanceLocator: process.env.REACT_APP_CHATKIT_INSTANCE_LOCATOR,
-		userId, // We have stored googleId as userId
-		tokenProvider
-	})
-	// Register global events
-		.connect({
-			onAddedToRoom: actions.subscribeToRoom,
-			onRemovedFromRoom: actions.removedFromRoom,
-			onPresenceChanged: actions.setUserPresence,
-			onRoomDeleted: actions.roomDeleted
-		})
-		.then(user => {
-			// Subscribe to all rooms the user is a member of
-			Promise.all(
-				user.rooms.map(room =>
-					user.subscribeToRoom({
-						roomId: room.id,
-						messageLimit: 5,
-						hooks: {
-							onUserJoined: actions.refresh,
-							onMessage: actions.addMessage,
-							onUserLeft: actions.refresh, // force update app on user removal
-							onUserStartedTyping: actions.isTyping,
-							onUserStoppedTyping: actions.notTyping
-						}
-					})
-				)
-			).then(rooms => {
-				actions.setUser(user);
-				// Join the first room in the users room list
-				user.rooms.length > 0 && actions.joinRoom(user.rooms[0]);
-				// Notify app that Connection is successfull
-				actions.connected(user.rooms);
-			});
-		})
-		.catch(error => {
-			if (error.info) {
-				notification.error({
-					message: error.info.error,
-					description: error.info.error_description,
-					onClose: () => {
-						console.log("inside onClose");
-						// Clearn localstorage
-						localStorage.removeitem("slackUser");
-					}
-				});
-			} else actions.error(actions);
-		});
+  new Chatkit.ChatManager({
+    instanceLocator: process.env.REACT_APP_CHATKIT_INSTANCE_LOCATOR,
+    userId, // We have stored googleId as userId
+    tokenProvider,
+  })
+  // Register global events
+    .connect({
+      onAddedToRoom: actions.subscribeToRoom,
+      onRemovedFromRoom: actions.removedFromRoom,
+      onPresenceChanged: actions.setUserPresence,
+      onRoomDeleted: actions.roomDeleted,
+    })
+    .then((user) => {
+      // Subscribe to all rooms the user is a member of
+      Promise.all(
+        user.rooms.map(room => user.subscribeToRoom({
+            roomId: room.id,
+            messageLimit: 5,
+            hooks: {
+              onUserJoined: actions.refresh,
+              onMessage: actions.addMessage,
+              onUserLeft: actions.refresh, // force update app on user removal
+              onUserStartedTyping: actions.isTyping,
+              onUserStoppedTyping: actions.notTyping,
+            },
+          }),),
+      ).then((rooms) => {
+        actions.setUser(user);
+        // Join the first room in the users room list
+        user.rooms.length > 0 && actions.joinRoom(user.rooms[0]);
+        // Notify app that Connection is successfull
+        actions.connected(user.rooms);
+      });
+    })
+    .catch((error) => {
+      if (error.info) {
+        notification.error({
+          message: error.info.error,
+          description: error.info.error_description,
+          onClose: () => {
+            // Clearn localstorage
+            localStorage.removeitem('slackUser');
+          },
+        });
+      } else actions.error(actions);
+    });
 };
