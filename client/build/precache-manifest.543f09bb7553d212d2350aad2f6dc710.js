86772ebf787d98e8a596b03ad2c8c9bd1802e30c
Amit <amitptil321@gmail.com>
2019-07-10 18:07:59 +0530

    Removed build folder from gitignore

---
+++ client/build/precache-manifest.543f09bb7553d212d2350aad2f6dc710.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e414580a2b7fa2d01e0cedf64863eb6a",
+    "url": "/React-TMDB/index.html"
+  },
+  {
+    "revision": "6602e4c9f1c8c6232c82",
+    "url": "/React-TMDB/static/css/4.7af64b5f.chunk.css"
+  },
+  {
+    "revision": "233871a48fdd1aadd18e",
+    "url": "/React-TMDB/static/css/7.6f494bfd.chunk.css"
+  },
+  {
+    "revision": "43d01344c2a50fd2f4eb",
+    "url": "/React-TMDB/static/css/8.4b0201db.chunk.css"
+  },
+  {
+    "revision": "55406ca953cb5ca743c9",
+    "url": "/React-TMDB/static/css/9.5d818086.chunk.css"
+  },
+  {
+    "revision": "bcca4d51804042e56baa",
+    "url": "/React-TMDB/static/css/main.7c9ae454.chunk.css"
+  },
+  {
+    "revision": "980e394cc5504b736bce",
+    "url": "/React-TMDB/static/js/0.9d62f8ec.chunk.js"
+  },
+  {
+    "revision": "7f2962b8db066bdafa51",
+    "url": "/React-TMDB/static/js/1.8d2a7a11.chunk.js"
+  },
+  {
+    "revision": "5f9c2ab8a2bc7cf8168b",
+    "url": "/React-TMDB/static/js/10.4a31b6a3.chunk.js"
+  },
+  {
+    "revision": "9dfa9973cff491e186d1",
+    "url": "/React-TMDB/static/js/11.37bc0161.chunk.js"
+  },
+  {
+    "revision": "aab45f6b82432221ffa1",
+    "url": "/React-TMDB/static/js/12.7741e7e1.chunk.js"
+  },
+  {
+    "revision": "9f14645548d6db58077f",
+    "url": "/React-TMDB/static/js/13.1dd7df32.chunk.js"
+  },
+  {
+    "revision": "6602e4c9f1c8c6232c82",
+    "url": "/React-TMDB/static/js/4.f0becaee.chunk.js"
+  },
+  {
+    "revision": "fb373c18fd12c5ef8686",
+    "url": "/React-TMDB/static/js/5.aabc7c80.chunk.js"
+  },
+  {
+    "revision": "60732836461eb2238fd6",
+    "url": "/React-TMDB/static/js/6.f308ccde.chunk.js"
+  },
+  {
+    "revision": "233871a48fdd1aadd18e",
+    "url": "/React-TMDB/static/js/7.1c66846e.chunk.js"
+  },
+  {
+    "revision": "43d01344c2a50fd2f4eb",
+    "url": "/React-TMDB/static/js/8.50b6e24c.chunk.js"
+  },
+  {
+    "revision": "55406ca953cb5ca743c9",
+    "url": "/React-TMDB/static/js/9.ed6f0c58.chunk.js"
+  },
+  {
+    "revision": "bcca4d51804042e56baa",
+    "url": "/React-TMDB/static/js/main.22f12c20.chunk.js"
+  },
+  {
+    "revision": "038a87f157faa15aa5e2",
+    "url": "/React-TMDB/static/js/runtime~main.aa075070.js"
+  },
+  {
+    "revision": "9f0be94467d55d6c65ede646397b3f74",
+    "url": "/React-TMDB/static/media/32.9f0be944.png"
+  }
+]);
