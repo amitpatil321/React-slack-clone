253187cd9401d1243c4a73e1005aae370da3c09e
Amit <amitptil321@gmail.com>
2019-07-11 19:05:28 +0530

    Removed extra cors package entry

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const express = require('express');
-var cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || process.env.NODE_PORT;
