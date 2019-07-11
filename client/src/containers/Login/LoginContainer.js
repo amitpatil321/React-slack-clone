f6672d3421ebb2c73ee68a50c166a82c9e213732
Amit <amitptil321@gmail.com>
2019-07-11 12:34:41 +0530

    Add console log for testing

--- client/src/containers/Login/LoginContainer.js
+++ client/src/containers/Login/LoginContainer.js
@@ -13,32 +13,34 @@ export default class LoginContainer extends Component {
     }
 
     // Handle google login success response
-    _onSuccess = (googleResponse) => {
-      const { profileObj } = googleResponse;
-      if (profileObj) {
-        const self = this;
-        axios.post(`${CONFIG.SERVER}/user`, profileObj)
-          .then((response) => {
-            localStorage.setItem('slack', JSON.stringify(googleResponse.profileObj));
-            self.setState({ loginSuccess: true });
-          })
-          .catch((error) => {
-            // throw error
-            self.setState({
-              error: <Alert type="error" title="error" message="Failed to create user!, Please try again." />,
-            });
+  _onSuccess = (googleResponse) => {
+    console.log(googleResponse);
+    const { profileObj } = googleResponse;
+    if (profileObj) {
+      const self = this;
+      axios.post(`${CONFIG.SERVER}/user`, profileObj)
+        .then((response) => {
+          localStorage.setItem('slack', JSON.stringify(googleResponse.profileObj));
+          self.setState({ loginSuccess: true });
+        })
+        .catch((error) => {
+          // throw error
+          self.setState({
+            error: <Alert type="error" title="error" message="Failed to create user!, Please try again." />,
           });
-      }
+        });
     }
+  }
 
     _onFailure = (err) => {
-      // throw error
+      console.log(err);
       this.setState({
         error: <Alert type="error" title="error" message="Failed to create user!, Please try again." />,
       });
     }
 
     render() {
+      console.log(CONFIG);
       const { loginSuccess, error } = this.state;
       if (loginSuccess) { return <Redirect to="/" />; }
       return (
